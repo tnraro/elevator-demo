26478bd5aee7aa133895ab0392937a11c5a810ad
tnraro <deokji53@gmail.com>
2024-06-24 18:50:36 +0900

    add simple elevators

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+
 function App() {
+  const [elevators, setElevators] = useState([1, 1, 1]);
+
+  useEffect(() => {
+    // for test
+    const timer = setInterval(() => {
+      setElevators(Array.from({ length: 3 }).map(() => Math.random() * 15 + 1 | 0));
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    }
+  }, []);
+
   return (
     <>
-      WIP
+      <div className="elevator-container">
+        {elevators.map(floor => (
+          <div className="elevator-shaft" style={{ "--height": 15 }}>
+            <div className="elevator" style={{ "--current-floor": floor }}>{floor}</div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
